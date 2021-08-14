dc3637c649c2932c0b3fb55efa648ae021027306
raylu <90059+raylu@users.noreply.github.com>
2021-08-13 21:49:59 -0700

    display data

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+(async () => {
+	const response = await fetch('times.json');
+	const times = await response.json();
+
+	const text = times.map(([ts, ms]) => `${ts}\t${ms} ms`).join('\n');
+	document.querySelector('section').innerText = text;
+
+	const chart = c3.generate({
+		'bindto': '#chart',
+		'data': {
+			'type': 'area',
+			'x': 'x',
+			'columns': [],
+		},
+		'axis': {
+			'x': {
+				'type': 'timeseries',
+				'tick': {'format': '%a %H:%M'}, // https://github.com/d3/d3-time-format#locale_format
+			},
+		},
+	});
+	const columns = [
+		['x', ...times.map(([ts, ms]) => new Date(ts + 'Z'))],
+		['ms', ...times.map(([ts, ms]) => ms)],
+	];
+	chart.load({'columns': columns});
+})();
